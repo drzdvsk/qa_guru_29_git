36b1c003380f1ad9013996f2daacc71c72332bb8
Andrei Drazdouski <117362052+drzdvsk@users.noreply.github.com>
2024-07-03 00:45:15 +0300

    Update Main.java

--- src/main/java/ru/drzdvsk/Main.java
+++ src/main/java/ru/drzdvsk/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        System.out.printf(msg);
+        System.out.printf(ms);
         
 private static String msg = "Hello!?!?"
 
